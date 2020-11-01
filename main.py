aee7f3fbd1930e30bbebbade890ec4b8fa06b13f
Luis Osta <luisosta1999@gmail.com>
2020-10-31 21:28:46 -0500

    removed unnecessary prints

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 """
 Imports
 """
-import numpy as np
 from neural_network import create_chatbot, create_model
 from chat_gui import initialize_chat
 from processor import process_raw_data, create_training_data
@@ -12,10 +11,6 @@ INTENTS_FILE_PATH = "./data/intents.json"
 
 training_data, output_labels, words, labels = create_training_data(
     *process_raw_data(INTENTS_FILE_PATH))
-
-
-print("Training: ", training_data)
-print("Output: ", output_labels)
 
 model = create_model(training_data, output_labels)
 
