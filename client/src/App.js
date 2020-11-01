11c73aeeac96f8c8f32df002add4d941096a1cf4
Luis Osta <luisosta1999@gmail.com>
2020-11-01 00:34:01 -0500

    added UI

---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { Launcher } from "react-chat-window";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+const initialValues = [
+  {
+    author: "Them",
+    type: "text",
+    data: {
+      text:
+        "Welcome! Here you can ask whatever questions you want about finance",
+    },
+  },
+  {
+    author: "Them",
+    type: "text",
+    data: {
+      text: "Ask any question you want.",
+    },
+  },
+];
+
+function App() {
+  const [messageList, setMessageList] = useState(initialValues);
+
+  const handleMessageSent = async (message) => {
+    const { data } = await axios.post("/chatbot/response", {
+      data: { text: message.data.text },
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+
+    setMessageList([
+      ...messageList,
+      message,
+      {
+        author: "Them",
+        type: "text",
+        data: {
+          text: data,
+        },
+      },
+    ]);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Launcher
+        agentProfile={{
+          teamName: "CS4395 Chatbot",
+          imageUrl:
+            "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png",
+        }}
+        onMessageWasSent={handleMessageSent}
+        messageList={messageList}
+        showEmoji={false}
+        isOpen={true}
+      />
+    </div>
+  );
+}
+
+export default App;
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 """
 Imports
 """
+import json
+from flask import Flask, send_from_directory, request
 import os
 import logging
 import subprocess
+
+from werkzeug.exceptions import BadRequest
 from neural_network import create_chatbot, create_model
 from chat_gui import initialize_chat
 from processor import process_raw_data, create_training_data
 
-
+app = Flask(__name__, static_folder="client/build/")
 logging.getLogger('tflearn').setLevel(logging.CRITICAL)
 logging.getLogger('tensorflow').setLevel(logging.CRITICAL)
 
@@ -16,11 +20,41 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 INTENTS_FILE_PATH = "./data/intents.json"
 
 
-subprocess.call('clear')
-
+# subprocess.call('clear')
 training_data, output_labels, words, labels = create_training_data(
     *process_raw_data(INTENTS_FILE_PATH))
 
 model = create_model(training_data, output_labels)
 
-initialize_chat(create_chatbot(model, labels, words, INTENTS_FILE_PATH))
+get_chatbot_response = create_chatbot(
+    model, labels, words, INTENTS_FILE_PATH)
+
+
+@app.route("/", defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_react(path):
+    """
+        Docstring
+    """
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route("/chatbot/response", methods=["POST"])
+def send_chatbot_response():
+    """
+        Docstring
+    """
+    try:
+        text = request.get_json()['data']['text']
+        return get_chatbot_response(text)
+    except BadRequest as error:
+        print(error)
+        return "Error"
+
+
+if __name__ == "__main__":
+    subprocess.call('clear')
+    app.run(use_reloader=True, port=5000, threaded=True)
